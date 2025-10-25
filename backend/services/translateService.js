01a9ba6e1e96151f37600b28094eb794569263d6
azizu06 <ab725492@ucf.edu>
2025-10-25 03:46:55 -0400

    Connect backend to Google Translate API and return translated text

---
+++ backend/services/translateService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export async function translateText(text, targetLang) {
+    try {
+        const response = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${process.env.GOOGLE_TRANSLATE_KEY}`,
+        {
+            q: text,
+            target: targetLang
+        }
+    );
+
+    const translatedText = response.data.data.translations[0].translateText;
+    return translatedText;
+    } catch(error) {
+        console.error("Translation API error::", error.response?.data || error.message);
+
+        return "Error: Unable to translate text right now.";
+    }
+}
