7894087e499665277973db0eef3d4b6971145f0b
azizu06 <ab725492@ucf.edu>
2025-10-25 17:50:43 -0400

    Remove comments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-// Load environment variables before importing any modules that depend on them
 import "dotenv/config";
 import adviceRoutes from "./routes/advice.js";
 import userRoutes from "./routes/user.js";
@@ -20,6 +19,6 @@ app.get("/", (req, res) => {
     res.send("FinBridge backend is running.");
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
