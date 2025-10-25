9a138bca3e7415249d8cddbf5769d0d6ce18f24e
joet <205975513+joetmp4@users.noreply.github.com>
2025-10-25 03:00:49 -0400

    Initialized React Project. Installed i18next and begun the useTranslation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'react-i18next';
+import './App.css';
+
+function App() {
+
+  const {t} = useTranslation();
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
