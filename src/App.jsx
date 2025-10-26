acd28726afee5ac8a25b526f4a5b45999d1d299c
azizu06 <ab725492@ucf.edu>
2025-10-25 23:30:30 -0400

    Update app file to fit backend configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import { useTranslation } from 'react-i18next';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import Header from './Header.jsx';
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard.jsx';
 import Home from './Home.jsx';
+import Dashboard from './Dashboard.jsx';
 import About from './About.jsx';
->>>>>>> dev
 
 function App() {
-
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  }
-
   return (
-    <>
-      <div className="App bg-gray-100 min-h-screen">
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Router>
-      </div>
-    </>
-  )
+    <div className="App bg-gray-100 min-h-screen">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
