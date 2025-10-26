a99220fa289bef08e93467090cf37bc82b81b921
azizu06 <ab725492@ucf.edu>
2025-10-26 04:07:12 -0400

    Import extra translation fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "./i18n.js";
+import './i18n.js'
+import { LocaleProvider } from './context/LocaleContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LocaleProvider>
+      <App />
+    </LocaleProvider>
   </StrictMode>,
 )
